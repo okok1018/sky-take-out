d773bd7abf21561f7602c321353846bb6272a7fa
okok1018 <2843674798@qq.com>
2025-12-24 20:12:56 +0800

    测试git

---
+++ sky-server/src/main/java/com/sky/gitPull/gitPull.java
@@ -0,0 +1,13 @@
+package com.sky.gitPull;
+
+public class gitPull {
+
+        //System.out.println("测试git拉代码，推代码");
+//test01
+public int gitPullTest() {
+    System.out.println("测试git拉代码，推代码");
+   return 1;
+
+    }
+}
+
